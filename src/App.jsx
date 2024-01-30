35e4057ea905339045a9460249fd778d8ac09420
IvanKent <ivankentmalate.landicho@bicol-u.edu.ph>
2024-01-30 16:00:33 +0800

    scoreboard

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
 import GalleryTrial from "./GalleryTrial"
 import Pointer from './Pointer'
 import FormInput from './FormInput'
+import ScoreBoard from './ScoreBoard'
 
 function App() {
   return (
     <div>
       {/* <GalleryTrial/> */}
       {/* <Pointer/> */}
-      <FormInput/>
+      {/* <FormInput/> */}
+      <ScoreBoard/>
     </div>
   )
 }
